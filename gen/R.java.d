88194dc953f1d7cafecc640ae95110a7280da1d4
jim syyap <jimsyyap@gmail.com>
2014-11-20 14:05:00 +1300

    removed readme

--- gen/R.java.d
+++ gen/R.java.d
@@ -5,4 +5,8 @@
 /home/jim/android/myfirstapp/res/values/strings.xml \
 /home/jim/android/myfirstapp/res/layout/main.xml \
 /home/jim/android/myfirstapp/res/drawable-ldpi/ic_launcher.png \
+/home/jim/android/myfirstapp/bin/res/drawable-mdpi/ic_launcher.png \
+/home/jim/android/myfirstapp/bin/res/drawable-hdpi/ic_launcher.png \
+/home/jim/android/myfirstapp/bin/res/drawable-xhdpi/ic_launcher.png \
+/home/jim/android/myfirstapp/bin/res/drawable-ldpi/ic_launcher.png \
 /home/jim/android/myfirstapp/bin/AndroidManifest.xml \
